8a368bcb6881ec335424f7f85d8bcdbce007d947
wylie@dukeofcheese.com <wylie@dukeofcheese.com>
2013-08-21 19:08:03 -0400

    adding master

--- master.php
+++ master.php
@@ -1,12 +1,9 @@
 <?php
 	
-	//define('DOCUMENT_ROOT', dirname(realpath(__FILE__)).'../');
-	
 	$appName = 'Bananas';
 	
 	function breadcrumb() {
 		
-		//echo 'Dashboard' . getcwd();
 		echo 'Dashboard / ' . basename(getcwd());
 		
 	}
